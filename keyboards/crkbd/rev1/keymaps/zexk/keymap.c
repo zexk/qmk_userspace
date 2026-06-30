8c3bea65e6c1668a3e40671529991c8121fa2edc
zexk <bouraouiochi@gmail.com>
2026-06-30 20:32:09 +0200

    keep semicolon as plain key
    LGUI on right hand dropped; GUI is still accessible via left A hold.

--- keyboards/crkbd/rev1/keymaps/zexk/keymap.c
+++ keyboards/crkbd/rev1/keymaps/zexk/keymap.c
@@ -8,7 +8,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         //,-----------------------------------------------------.    ,-----------------------------------------------------.
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,         KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
         //|--------+--------+--------+--------+--------+--------|    |--------+--------+--------+--------+--------+--------|
-        KC_ESC,  LGUI_T(KC_A), LALT_T(KC_S), LSFT_T(KC_D), LCTL_T(KC_F), KC_G,   KC_H,    RCTL_T(KC_J), RSFT_T(KC_K), LALT_T(KC_L), LGUI_T(KC_SCLN), KC_BSPC,
+        KC_ESC,  LGUI_T(KC_A), LALT_T(KC_S), LSFT_T(KC_D), LCTL_T(KC_F), KC_G,   KC_H,    RCTL_T(KC_J), RSFT_T(KC_K), LALT_T(KC_L), KC_SCLN, KC_BSPC,
         //|--------+--------+--------+--------+--------+--------|    |--------+--------+--------+--------+--------+--------|
         KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,         KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_DEL,
         //|--------+--------+--------+--------+--------+--------|    |--------+--------+--------+--------+--------+--------|
